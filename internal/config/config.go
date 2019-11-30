6e4e8085a8bfd1f4edfa082649e0a0c8ff1b84c0
Graeme Lawes <graemelawes@gmail.com>
2019-11-30 16:25:17 -0500

    Initial commit: go binary to parse config files
    Config file format inherited from https://github.com/zvonkok/oci-decorator

---
+++ internal/config/config.go
@@ -0,0 +1,97 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var defaultConfigDir = "/etc/oci-injector-hook"
+var configExt = ".json"
+
+type InjectorConfig struct {
+	Name           string
+	Version        string                     `mapstructure:"version"`
+	LogLevel       string                     `mapstructure:"log_level"`
+	ActivationFlag string                     `mapstructure:"activation_flag"`
+	DriverFeature  []string                   `mapstructure:"driver_feature"`
+	Inventory      map[string]InventoryConfig `mapstructure:"inventory"`
+}
+
+type InventoryConfig struct {
+	Devices     []string `mapstructure:"devices"`
+	Binaries    []string `mapstructure:"binaries"`
+	Libraries   []string `mapstructure:"libraries"`
+	Directories []string `mapstructure:"directories"`
+	Misc        []string `mapstructure:"miscellaneous"`
+}
+
+// visitConfigDir returns files in a directory with the extension configExt
+// from https://flaviocopes.com/go-list-files/
+func visitConfigDir(files *[]string) filepath.WalkFunc {
+	return func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if filepath.Ext(path) == configExt {
+			*files = append(*files, path)
+		}
+		return nil
+	}
+}
+
+// GetVipers returns a map of config name -> *viper.Viper config objects
+func GetVipers() map[string]*viper.Viper {
+	log.Printf("oci-injector-hook: getting configs")
+
+	configDir, ok := os.LookupEnv("OCI_INJECTOR_CONFIG_DIR")
+	if !ok {
+		configDir = defaultConfigDir
+	}
+
+	var configFiles []string
+	// var configs []*InjectorConfig
+	vipers := make(map[string]*viper.Viper)
+
+	// get config files in configDir
+	err := filepath.Walk(configDir, visitConfigDir(&configFiles))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range configFiles {
+		configName := strings.TrimSuffix(file, configExt)
+
+		v := viper.New()
+		v.SetConfigName(configName)
+		v.AddConfigPath(configDir)
+
+		if err := v.ReadInConfig(); err != nil {
+			log.Fatal("couldn't read config: %s", err)
+		}
+
+		vipers[configName] = v
+	}
+
+	return vipers
+}
+
+func GetConfigs() []*InjectorConfig {
+	var configs []*InjectorConfig
+	for name, v := range GetVipers() {
+		var config InjectorConfig
+
+		if err := v.Unmarshal(&config); err != nil {
+			log.Fatal("couldn't unmarshal config: %s", err)
+		}
+
+		config.Name = name
+
+		configs = append(configs, &config)
+	}
+
+	return configs
+}
