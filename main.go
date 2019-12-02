b059f6ccd76df08f598c49c17f0d09fc47381546
Graeme Lawes <graemelawes@gmail.com>
2019-12-01 19:49:20 -0500

    fix log stubs for simplified config

--- main.go
+++ main.go
@@ -10,17 +10,11 @@ func main() {
 
 	configs := config.GetConfigs()
 	for _, config := range configs {
-		log.Printf("config.Name=%s", config.Name)
-		log.Printf("config.Version=%s", config.Version)
-		log.Printf("config.ActivationFlag=%s", config.ActivationFlag)
-		log.Printf("config.DriverFeature=%s", config.DriverFeature)
-
-		for name, inv := range config.Inventory {
-			log.Printf("config.Inventory[%s].Devices=%s", name, inv.Devices)
-			log.Printf("config.Inventory[%s].Binaries=%s", name, inv.Binaries)
-			log.Printf("config.Inventory[%s].Libraries=%s", name, inv.Libraries)
-			log.Printf("config.Inventory[%s].Directories=%s", name, inv.Directories)
-			log.Printf("config.Inventory[%s].Misc=%s", name, inv.Misc)
-		}
+		log.Printf("configs[%s].ActivationFlag=%s", config.Name, config.ActivationFlag)
+		log.Printf("configs[%s].Devices=%s", config.Name, config.Devices)
+		log.Printf("configs[%s].Binaries=%s", config.Name, config.Binaries)
+		log.Printf("configs[%s].Libraries=%s", config.Name, config.Libraries)
+		log.Printf("configs[%s].Directories=%s", config.Name, config.Directories)
+		log.Printf("configs[%s].Misc=%s", config.Name, config.Misc)
 	}
 }
