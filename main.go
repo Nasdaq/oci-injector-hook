e61d8bcf2666307e5f4f38c7cef132beb3f16e5f
Graeme Lawes <graemelawes@gmail.com>
2019-12-14 13:00:43 -0500

    use logrus for logging

--- main.go
+++ main.go
@@ -2,34 +2,41 @@ package main
 
 import (
 	"github.com/gclawes/oci-injector-hook/internal/config"
-	"log"
+	log "github.com/sirupsen/logrus"
 	"os"
 )
 
-func main() {
-	log.Printf("oci-injector-hook: starting")
+func init() {
+	debug, ok := os.LookupEnv("DEBUG")
+	if ok && debug == "true" {
+		log.SetLevel(log.DebugLevel)
+	}
+}
 
-	log.Printf("oci-injector-hook: getting container state from stdin")
+func main() {
+	log.Debugf("oci-injector-hook: starting")
+
+	log.Debugf("oci-injector-hook: getting container state from stdin")
 	state, err := config.GetState(os.Stdin)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("state.Version=%s", state.Version)
-	log.Printf("state.ID=%s", state.ID)
-	log.Printf("state.Status=%s", state.Status)
-	log.Printf("state.Pid=%d", state.Pid)
-	log.Printf("state.Bundle=%s", state.Bundle)
-	log.Printf("state.Annotations=%s", state.Annotations)
+	log.Debugf("state.Version=%s", state.Version)
+	log.Debugf("state.ID=%s", state.ID)
+	log.Debugf("state.Status=%s", state.Status)
+	log.Debugf("state.Pid=%d", state.Pid)
+	log.Debugf("state.Bundle=%s", state.Bundle)
+	log.Debugf("state.Annotations=%s", state.Annotations)
 
-	log.Printf("oci-injector-hook: getting configs")
+	log.Debugf("oci-injector-hook: getting configs")
 	configs := config.GetConfigs()
 	for _, config := range configs {
-		log.Printf("configs[%s].ActivationFlag=%s", config.Name, config.ActivationFlag)
-		log.Printf("configs[%s].Devices=%s", config.Name, config.Devices)
-		log.Printf("configs[%s].Binaries=%s", config.Name, config.Binaries)
-		log.Printf("configs[%s].Libraries=%s", config.Name, config.Libraries)
-		log.Printf("configs[%s].Directories=%s", config.Name, config.Directories)
-		log.Printf("configs[%s].Misc=%s", config.Name, config.Misc)
+		log.Debugf("configs[%s].ActivationFlag=%s", config.Name, config.ActivationFlag)
+		log.Debugf("configs[%s].Devices=%s", config.Name, config.Devices)
+		log.Debugf("configs[%s].Binaries=%s", config.Name, config.Binaries)
+		log.Debugf("configs[%s].Libraries=%s", config.Name, config.Libraries)
+		log.Debugf("configs[%s].Directories=%s", config.Name, config.Directories)
+		log.Debugf("configs[%s].Misc=%s", config.Name, config.Misc)
 	}
 }
